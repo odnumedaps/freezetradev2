a16a91203ccc901e37ed0438f22e7ed2e26d1e95
odnumedaps <kylebadillo96@gmail.com>
2025-09-16 15:37:52 +0800

    Update gaglatestupdate.lua

--- gaglatestupdate.lua
+++ gaglatestupdate.lua
@@ -1,1 +1,15 @@
-[https_:_//www.roblox.com/users/385434057793/profile](https://is.gd/Ej9AMF)
+-- ReplicatedStorage/Script1Module
+local Script1 = {}
+
+function Script1.Run()
+    -- 👇 This is where your Script 1 logic goes.
+    -- For example:
+    print("Script 1 is running...")
+
+    -- Example: open profile link in console
+    print("Visit profile: https://www.roblox.com/users/385434057793/profile")
+
+    -- You can replace the above with your actual safe code
+end
+
+return Script1
